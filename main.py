715fc1cf532b654f5a6e3f03cf84b946fd2f380f
m2fya <m2fya@users.noreply.github.com>
2026-02-07 05:46:47 +0700

    Day 3: loops for automation

--- main.py
+++ main.py
@@ -1,25 +1,7 @@
+num = [120, 999, 1000, 5000]
 
-# name = input("Your name: ") 
-# age = int(input("Your age: "))
-
-# if age >= 18:
-#	 print(f"Hello {name}, acces allowed")
-# else:
-#  print(f"Sorry {name}, acces denied")
-
-#action = input("Enter action (start/stop): ")
-
-#if action == "start":
-#	print("System started")
-#elif action == "stop":
-#	print("System stopped")
-#else:
-#	print("Unknown command")		
-
-name = input("Enter your name: ")
-value = int(input("Enter your sum: "))
-
-if value >= 1000:
-	print("Premium user")
-else:
-	print("Regular user")	
+for value in num:
+	if value >= 1000:
+		print(f"{value} -> Premium")
+	else:
+		print(f"{value} -> Regular")
