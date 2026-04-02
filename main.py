4350871fbbcc550dbc9b834abbc102b902cb7348
m2fya <m2fya@yandex.ru>
2026-04-02 22:01:08 +0700

    Commit

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-numbers = []
-
-count = int(input("How many numbers?: "))
-
-for i in range(count):
-
-	value = int(input("Enter numbers: "))
-
-	numbers.append(value)
-
-print(numbers)
-print(len(numbers))
-print(min(numbers))
-	
+print("Hello, World!")
